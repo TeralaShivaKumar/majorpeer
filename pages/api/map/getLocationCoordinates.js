b512c5828d16e296b826fe27877c0b1120c47bd9
TeralaShivaKumar <65031455+TeralaShivaKumar@users.noreply.github.com>
2022-09-10 23:21:03 +0530

    Update getLocationCoordinates.js

--- pages/api/map/getLocationCoordinates.js
+++ pages/api/map/getLocationCoordinates.js
@@ -1,5 +1,5 @@
 const getLocationCoordinates = async (req, res) => {
-  const mapboxUrl = `${process.env.MAPBOX_PLACES_API_URL}/${req.body.location}.json?access_token=${process.env.pk.eyJ1Ijoic2hpdmFrdW1hcjEwMCIsImEiOiJjbDd3NXlmZW8waWhkM3htdmo1bGU3YjZ1In0.WHT-Jy9DUQzqDV77FO0Ypg}`
+  const mapboxUrl = `${process.env.mapbox://styles/shivakumar100/cl7w7b8ap000l15qug7030l3k}/${req.body.location}.json?access_token=${process.env.pk.eyJ1Ijoic2hpdmFrdW1hcjEwMCIsImEiOiJjbDd3NXlmZW8waWhkM3htdmo1bGU3YjZ1In0.WHT-Jy9DUQzqDV77FO0Ypg}`
 
   try {
     const response = await fetch(mapboxUrl)
